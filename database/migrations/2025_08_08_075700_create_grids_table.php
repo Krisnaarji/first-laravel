7128ee2f491473b1ab6f0022e5007ba2d183796c
Nasana <150105128+Krisnaarji@users.noreply.github.com>
2025-08-10 02:56:53 +0800

    feat: add migration for grids table with driver, constructor, and image path

--- database/migrations/2025_08_08_075700_create_grids_table.php
+++ database/migrations/2025_08_08_075700_create_grids_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('grids', function (Blueprint $table) {
             $table->id();
+            $table->string("driver_name");
+            $table->string("constructor");
+            $table->string("path");
             $table->timestamps();
         });
     }
